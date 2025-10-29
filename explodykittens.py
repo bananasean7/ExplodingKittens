adbf2345149bafe2da47f12ca52104a047a36eff
bananasean7 <bananasean7@gmail.com>
2025-10-29 13:03:05 +0000

    modified:   explodykittens.py

--- explodykittens.py
+++ explodykittens.py
@@ -8,10 +8,10 @@ def main():
     player2 = Player(deck1, 2)
     player3 = Player(deck1, 3)
     print()
+    # add more players here, then add them to the loop.
     while True:
         player1.take_turn(player2)
-        player2.take_turn(player3)
-        player3.take_turn(player1)
+        player2.take_turn(player1)
     print("THE GAME IS OVER") # It'll be a long time before this function runs lol
 
 if __name__ == "__main__":
